bd6d702e3ea68f5e5757cd13d75632a84facf335
Gaston Celis <gastoncelis09@gmail.com>
2024-03-03 13:15:46 -0300

    agregado de botones de accion header

--- src/components/pokeHeader/index.tsx
+++ src/components/pokeHeader/index.tsx
@@ -1,14 +1,26 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './pokeHeader.scss'
+import { faPowerOff } from '@fortawesome/free-solid-svg-icons';
 
 const PokeHeader = () => {
     return (
         <div className='container-pokeHeader'>
             <div className='container-circle-on'>
-                <div className='circle-on'></div>
+                <div className='circle-on'>
+                    <div className='circle-on-min-1'>
+                    </div>
+                    <div className='circle-on-min-2'>
+                    </div>
+                </div>
             </div>
-            <div className='circle-reset'></div>
-            <div className='circle-search'></div>
-            <div className='circle-close'></div>
+
+            <div className='container-circle-action'>
+                <button className='circle-reset'></button>
+                <button className='circle-search'></button>
+                <button className='circle-close'>
+                    <FontAwesomeIcon icon={faPowerOff} />
+                </button>
+            </div>
         </div>
     );
 };
